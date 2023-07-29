fb899108dcc564a04681998c393a7e59393ce2f6
Dennis Prediger <dennis.prediger@gmail.com>
2023-07-29 21:41:04 +0200

    Fix nested call arguments not getting validated

--- src/validate.rs
+++ src/validate.rs
@@ -10,10 +10,10 @@ pub enum ValidationResult {
 
 /// Validates `Variable` and `Call` [`Expressions`](crate::ast::Expression) by walking
 /// the tree and returning a [`ValidationResult`] on the first error.
-pub fn validate_env(env: &Environment, expr: &Expression) -> ValidationResult {
+pub fn validate_env(env: &Environment, expression: &Expression) -> ValidationResult {
     let mut result = ValidationResult::Valid;
 
-    match expr {
+    match expression {
         Expression::Unary { right, operator: _ } => result = validate_env(env, right),
         Expression::Binary {
             left,
@@ -34,12 +34,29 @@ pub fn validate_env(env: &Environment, expr: &Expression) -> ValidationResult {
             Some(function) => {
                 if function.arity != params.len() {
                     result = ValidationResult::ParamCountMismatch(function.arity, params.len());
+                } else {
+                    result = validate_expr_vec(env, params);
                 }
             }
             None => result = ValidationResult::MissingFunction(name.clone()),
         },
         Expression::Literal(_) => (),
     };
+
+    result
+}
+
+fn validate_expr_vec(env: &Environment, expressions: &Vec<Expression>) -> ValidationResult {
+    let mut result = ValidationResult::Valid;
+
+    for expr in expressions {
+        result = validate_env(env, expr);
+        if let ValidationResult::Valid = result {
+            // do nothing
+        } else {
+            break;
+        }
+    }
 
     result
 }
@@ -67,7 +84,7 @@ mod test {
     }
 
     #[test]
-    fn missing_variable() {
+    fn err_missing_variable() {
         let ast = Expression::Binary {
             left: Box::new(Expression::Literal(Value::Number(10.0))),
             right: Box::new(Expression::Variable("VAR_NAME".to_string())),
@@ -83,7 +100,7 @@ mod test {
     }
 
     #[test]
-    fn function_missing() {
+    fn err_function_missing() {
         let ast = Expression::Binary {
             left: Box::new(Expression::Literal(Value::Number(10.0))),
             right: Box::new(Expression::Call("max".to_string(), vec![])),
@@ -95,12 +112,12 @@ mod test {
         assert_eq!(ValidationResult::MissingFunction("max".to_string()), result);
     }
 
-    fn dummy_max(_params: Vec<Value>) -> Result<Value, String> {
+    fn dummy_function(_params: Vec<Value>) -> Result<Value, String> {
         Ok(Value::Boolean(true))
     }
 
     #[test]
-    fn function_params_mismatch() {
+    fn err_function_params_mismatch() {
         let ast = Expression::Binary {
             left: Box::new(Expression::Literal(Value::Number(10.0))),
             right: Box::new(Expression::Call("max".to_string(), vec![])),
@@ -108,10 +125,28 @@ mod test {
         };
 
         let mut env = Environment::default();
-        env.add_native_func("max", 2, dummy_max);
+        env.add_native_func("max", 2, dummy_function);
 
         let result = validate_env(&env, &ast);
 
         assert_eq!(ValidationResult::ParamCountMismatch(2, 0), result);
+    }
+
+    #[test]
+    fn err_function_nested_params() {
+        let ast = Expression::Call(
+            "func".to_string(),
+            vec![Expression::Variable("not_found".to_string())],
+        );
+
+        let mut env = Environment::default();
+        env.add_native_func("func", 1, dummy_function);
+
+        let result = validate_env(&env, &ast);
+
+        assert_eq!(
+            ValidationResult::MissingVariable("not_found".to_string()),
+            result
+        );
     }
 }
