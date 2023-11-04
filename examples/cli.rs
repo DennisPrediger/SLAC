296c6bb63b8077b3009ef6bc344c642931e13f00
Dennis Prediger <dennis.prediger@gmail.com>
2023-11-04 13:11:41 +0100

    Add `cli` example

---
+++ examples/cli.rs
@@ -0,0 +1,33 @@
+use std::{env, process::ExitCode};
+
+use slac::{Result, Value};
+
+fn execute(source: &str) -> Result<Value> {
+    let ast = slac::compile(&source)?;
+    let mut env = slac::StaticEnvironment::default();
+    slac::stdlib::extend_environment(&mut env);
+    slac::check_variables_and_functions(&env, &ast)?;
+
+    let result = slac::execute(&env, &ast)?;
+    Ok(result)
+}
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(source) = args.get(1) {
+        match execute(&source) {
+            Ok(result) => {
+                println!("{result}");
+                ExitCode::SUCCESS
+            }
+            Err(error) => {
+                println!("Error: {error}");
+                ExitCode::FAILURE
+            }
+        }
+    } else {
+        println!("Error: no script provided");
+        ExitCode::FAILURE
+    }
+}
